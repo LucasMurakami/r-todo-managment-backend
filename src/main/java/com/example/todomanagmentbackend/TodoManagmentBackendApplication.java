c779821380ba7651b329b96aa545bba89e22562c
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-10-24 18:24:34 -0300

    Model Mapper

--- src/main/java/com/example/todomanagmentbackend/TodoManagmentBackendApplication.java
+++ src/main/java/com/example/todomanagmentbackend/TodoManagmentBackendApplication.java
@@ -1,10 +1,17 @@
 package com.example.todomanagmentbackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoManagmentBackendApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoManagmentBackendApplication.class, args);
