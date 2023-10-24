79a6e824e3f722c2a0cd74c68c0f706094698324
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-10-24 16:06:22 -0300

    Projeto Iniciado

---
+++ src/main/java/com/example/todomanagmentbackend/TodoManagmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.todomanagmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagmentBackendApplication.class, args);
+	}
+
+}
