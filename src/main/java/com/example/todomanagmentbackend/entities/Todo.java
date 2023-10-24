29da2dc85a3555e798a168dc6a9b017a3f6290c5
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-10-24 18:23:13 -0300

    Entity Package

---
+++ src/main/java/com/example/todomanagmentbackend/entities/Todo.java
@@ -0,0 +1,30 @@
+package com.example.todomanagmentbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "todos")
+
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "desciption", nullable = false)
+    private String description;
+
+    private boolean completed;
+
+}
